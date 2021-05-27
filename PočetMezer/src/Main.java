8fbc662b5bc8149b95ccffa84c82418d03f4c94b
palicka2 <84902713+palicka2@users.noreply.github.com>
2021-05-27 10:43:40 +0200

    Pridani souboru

---
+++ PočetMezer/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	      File file = new File("M:\\Java IT\\PoèetMezer\\lm.txt"); 
+	      String loremIpsum = new String(Files.readAllBytes(Paths.get(file.getName())));
+	      
+	      int Mezery = 0;
+	      int Slova = 1;
+	      for (int i = 0; i < loremIpsum.length(); i++) {
+	    	  if(loremIpsum.charAt(i) == ' ') {
+	    		  Mezery++;
+	    		  Slova++;
+	    	  }
+		}
+	      System.out.println("text má " + Mezery + " mezer a " + Slova + " slov");	      
+	}
+
+}
